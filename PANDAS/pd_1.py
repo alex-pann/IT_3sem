724e117e79abcea491714fd833fc305f7504f0a6
alex-pann <alexandra.panasik@yandex.ru>
2021-10-24 23:44:41 +0300

    PANDAS task 1, 2

--- PANDAS/pd_1.py
+++ PANDAS/pd_1.py
@@ -7,7 +7,7 @@ path = str(input())
 data = pd.read_csv(path)
 n = data.shape[0] #количество строк в файле
 
-# задача №1: ищем три крупнейшик платежа (max.SUM)
+# задача №1: ищем три крупнейшик платежа (max SUM)
 # выбираем платежи из STATUS = OK
 data_ok = data.loc[data.STATUS == 'OK']
 print("#1: 3 самых крупных платежа: ")
@@ -16,4 +16,4 @@ print(data_ok.sort_values(by = 'SUM', ascending = False).iloc[0:3, :])
 # задача №2: ищем полную сумму платежей в Umbrella, Inc
 data_ok_umbr = data_ok.loc[data_ok.CONTRACTOR == "Umbrella, Inc"]
 print("#2: полная сумма платежей в Umbrella, Inc:")
-print(data_ok_umbr.describe().SUM[0])
+print(data_ok_umbr.SUM.sum())
---
+++ PANDAS/pd_2.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+print("Введите путь до файлов с данными: ") 
+path = str(input())
+
+# cчитывание данных из файла
+data = pd.read_csv(path)
+
+n = len(data.CARGO.unique()) # количество различных авиакомпаний
+
+# списки данных о количестве полетов, стоимости и массе
+# для построения графиков
+flights = []
+prices = []
+weight = []
+for i in range (n):
+    name = data.CARGO.unique()[i] # название авиакомании
+    data_sep = data[data.CARGO == name] # отделяем данные только по выбраной компании
+    print(
+        'Авиакомпания', name, '\n', 'Количество рейсов:', data_sep.CARGO.count(), '\n',
+        'Полная стоимость перевезенных грузов:', data_sep.PRICE.sum(), '\n','Полная масса перевезенных грузов:', data_sep.WEIGHT.sum(), '\n')
+    flights.append(data_sep.CARGO.count())
+    prices.append(data_sep.PRICE.sum())
+    weight.append(data_sep.WEIGHT.sum())
+
+# построение графиков
+fig, ax = plt.subplots(3, 1)
+ax[0].pie(flights, labels = data.CARGO.unique(), autopct='%1.1f%%', radius = 1.2)
+ax[0].set_title('Количество рейсов')
+
+ax[1].pie(prices, labels = data.CARGO.unique(), autopct='%1.1f%%', radius = 1.2)
+ax[1].set_title('Полная стоимость')
+
+ax[2].pie(weight, labels = data.CARGO.unique(), autopct='%1.1f%%', radius = 1.2)
+ax[2].set_title('Полная масса грузов')
+
+plt.show()
