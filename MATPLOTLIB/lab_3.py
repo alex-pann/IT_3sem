ff8619942f05f13f9d955ab8de84d820a217026b
alex-pann <alexandra.panasik@yandex.ru>
2021-10-26 19:13:25 +0300

    fixed lab_3

--- MATPLOTLIB/lab_3.py
+++ MATPLOTLIB/lab_3.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 print("Введите путь до файлов с данными: ") 
 path = str(input())
+path_data = path + 'students.csv' 
 
 preps = set()
 groups = set()
 
-with open(path, 'r') as f:
+with open(path_data, 'r') as f:
 
     # считываем имена всех преподавателей и номера всех групп
     for l in f.readlines(): 
@@ -26,7 +28,7 @@ with open(path, 'r') as f:
 f.close()
 
 
-with open(path, 'r') as f:
+with open(path_data, 'r') as f:
     # в каждой строке файла считываем преподавателя, группу, оценку
     for l in f.readlines():
         line = l.split(';')
@@ -39,68 +41,41 @@ with open(path, 'r') as f:
 
 f.close()
 
-fig, ax = plt.subplots(2, 1)
+fig, ax = plt.subplots(2, 1, figsize=(12, 8))
 
-# список имен преподавателей
 s = len(preps)
 prep_lb = []
 for i in range(0, s):
     prep_lb.append("prep" + str(i+1))
 
-# сортировка словаря marks per prep
-# (создание нового отсортированного словаря, 
-# потому что у исходного нет метода сортировки)
-sort_dict = {}
-for k in prep_lb:
-    sort_dict[k] = data1[1][k]
-
-ax[0].bar(prep_lb, sort_dict.values(), 0.6, label='1') # вне цикла строим диаграмму для первой оценки
-data = [d for d in sort_dict.values()]
+ax[0].bar(prep_lb, sorted(data1[1].values(), key = lambda key: data1[1].keys()), 0.6, label='1')
+data = [d for d in sorted(data1[1].values(), key = lambda key: data1[1].keys())]
 for i in range(2, 11):
-    sort_dict = {} # сортировка словаря повтаряется для каждой оценки i
-    for k in prep_lb:
-        sort_dict[k] = data1[i][k]
-
-    sort_dict_prev = {} # также сортируем данные предыдущей оценки
-    for k in prep_lb:
-        sort_dict_prev[k] = data1[i-1][k]
-    
-    data_prev = [d for d in sort_dict_prev.values()]
+    data_prev = [d for d in sorted(data1[i-1].values(), key = lambda key: data1[i-1].keys())]
     for j in range(0, len(data_prev)):
+        if i != 2:
             data[j] = data[j]+data_prev[j]
-    ax[0].bar(prep_lb, sort_dict.values(), 0.6, bottom=data, label=i)
-ax[0].legend()
+    ax[0].bar(prep_lb, sorted(data1[i].values(), key = lambda key: data1[i].keys()), 0.6, bottom=data, label=i)
+ax[0].yaxis.set_major_locator(ticker.MultipleLocator(3))
+ax[0].legend(loc='right')
 ax[0].set_title('Marks per prep')
 
-# список номеров групп
 s = len(groups)
 group_lb = []
 for i in range(0, s):
     group_lb.append(751 + i)
 
-# сортировка словаря для marks per group
-sort_dict_2 = {}
-
-for t in group_lb:
-    sort_dict_2[str(t)] = data2[1][str(t)]
-
-# вторая диаграмма строится аналогично первой
-ax[1].bar(group_lb, sort_dict_2.values(), 0.6, label='1') 
-data = [d for d in sort_dict_2.values()]
+ax[1].bar(group_lb, data2[1].values(), 0.6, label='1')
+data = [d for d in data2[1].values()]
 for i in range(2, 11):
-    sort_dict_2 = {}
-    for t in group_lb:
-        sort_dict_2[str(t)] = data2[i][str(t)]
-
-    sort_dict_prev = {}
-    for t in group_lb:
-        sort_dict_prev[str(t)] = data2[i-1][str(t)]
-    
-    data_prev = [d for d in sort_dict_prev.values()]
+    data_prev = [d for d in data2[i-1].values()]
     for j in range(0, len(data_prev)):
+        if i != 2:
             data[j] = data[j]+data_prev[j]
-    ax[1].bar(group_lb, sort_dict_2.values(), 0.6, bottom=data, label=i)
-ax[1].legend()
+    ax[1].bar(group_lb, data2[i].values(), 0.6, bottom=data, label=i)
+ax[1].yaxis.set_major_locator(ticker.MultipleLocator(3))
+ax[1].legend(loc='right')
 ax[1].set_title('Marks per group')
 
+plt.savefig(path + "graphs.png")
 plt.show()
